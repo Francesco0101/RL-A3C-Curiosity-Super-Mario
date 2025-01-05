ace0dad895a819868f6f30af41afd3313351cac5
Simone Palumbo <palumbosimone80@gmail.com>
2025-01-05 13:40:18 +0100

    added dense checkpoints and logs

--- plot.py
+++ plot.py
@@ -1,66 +1,4 @@
 
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# def moving_average(data, window_size):
-#     """
-#     Calculate the moving average of a list of numbers.
-#     :param data: List of numbers.
-#     :param window_size: Size of the moving window.
-#     :return: List of moving average values.
-#     """
-#     return np.convolve(data, np.ones(window_size) / window_size, mode='valid')
-
-# def plot(path_loss, path_distance, window_size=10):
-#     with open(path_loss, 'r') as f:
-#         lines = f.readlines()
-#         lines = lines[1:]
-#         ep_rewards = []
-#         ep_total_losses = []
-
-#         # Extract rewards and total losses from the file
-#         for line in lines:
-#             line = line.split()
-#             ep_rewards.append(float(line[3]))
-#             ep_total_losses.append(float(line[4]))
-
-#         # Calculate moving averages
-#         rewards_ma = moving_average(ep_rewards, window_size)
-#         total_losses_ma = moving_average(ep_total_losses, window_size)
-
-#         # Plot the moving average of rewards
-#         plt.plot(rewards_ma, label='Total Reward (Moving Average)')
-#         plt.legend()
-#         plt.savefig('rewards_ma.png')
-#         plt.close()
-
-#         # Plot the moving average of total losses
-#         plt.plot(total_losses_ma, label='Total Loss (Moving Average)')
-#         plt.legend()
-#         plt.savefig('total_losses_ma.png')
-#         plt.close()
-
-#     print("/////////////////////////////////////////////////////////////////////////////")
-
-#     with open(path_distance, 'r') as f:
-#         lines = f.readlines()
-#         lines = lines[1:]
-#         distances = []
-
-#         # Extract distances from the file
-#         for line in lines:
-#             line = line.split()
-#             distances.append(float(line[1]))
-
-#         # Calculate moving average of distances
-#         distances_ma = moving_average(distances, window_size)
-
-#         # Plot the moving average of distances
-#         plt.plot(distances_ma, label='Distance (Moving Average)')
-#         plt.legend()
-#         plt.savefig('distance_ma.png')
-#         plt.close()
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -90,16 +28,16 @@ def plot(path, window_size=10):
         lines = lines[1:]
         ep_total_losses = []
 
-        # Extract rewards and total losses from the file
+       
         for line in lines:
             line = line.split()
             ep_total_losses.append(float(line[4]))
 
-        # Calculate moving averages and standard deviations
+        
    
         total_losses_ma, total_losses_std = moving_average_and_std(ep_total_losses, window_size)
 
-        # Plot the moving average and standard deviation for total losses
+        
         plt.plot(total_losses_ma, label='Total Loss (Moving Average)', color='orange')
         plt.fill_between(
             range(len(total_losses_ma)),
@@ -120,16 +58,16 @@ def plot(path, window_size=10):
         distances = []
         rewards = []
 
-        # Extract distances from the file
+        
         for line in lines:
             line = line.split()
             distances.append(float(line[1]))
             rewards.append(float(line[0]))
 
-        # Calculate moving average and standard deviation for distances
+        
         distances_ma, distances_std = moving_average_and_std(distances, window_size)
         rewards_ma, rewards_std = moving_average_and_std(rewards, window_size)
-        # Plot the moving average and standard deviation for distances
+        
         plt.plot(distances_ma, label='Distance (Moving Average)', color='green')
         plt.fill_between(
             range(len(distances_ma)),
@@ -156,4 +94,4 @@ def plot(path, window_size=10):
         plt.close()
 
 
-plot('log/curiosity/no_reward/1_1/log_0', 100)
+plot('log/no_curiosity/dense/1_1/log_0', 100)
